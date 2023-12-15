43d2f19c360cbdd1ed88fc622458a59db0a498bb
vinisilva2020 <vinicius.2021318590@aluno.iffar.edu.br>
2023-12-15 19:14:59 -0300

    catua

--- config/conexao.php
+++ config/conexao.php
@@ -1,5 +1,13 @@
 <?php
 
 
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "banco-tcc-pablo";
 
-?>
+$conexao = mysqli_connect($servidor,$usuario,$senha,$banco);
+
+if ($conexao == FALSE) {
+    echo "ERRO de conexao".mysqli_connect_error();
+}
--- usuario/usuario.php
+++ usuario/usuario.php
@@ -1,5 +1,35 @@
 <?php
 
+session_start();
 
+include '../config/conexao.php';
+
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$telefone = $_POST['telefone'];
+$endereco = $_POST['endereco'];
+$senha = $_POST['senha'];
+
+$hash = password_hash($senha, PASSWORD_DEFAULT);
+
+
+
+$sql = "INSERT INTO usuario(nome,email,telefone,endereco,senha) VALUES ('$nome','$email','$telefone','$endereco','$hash')";
+
+
+$query = mysqli_query($conexao, $sql);
+
+if ($query) {
+    $_SESSION['title'] = "Cadastrado com sucesso!!";
+    $_SESSION['icon']  = "success";
+    header("location:../index-usuario.php");
+    die;
+}else {
+    $_SESSION['title'] = "Erro no cadastro".mysqli_error($conexao);
+    $_SESSION['icon']  = "error";
+    header("location:../index-usuario.php");
+    die;
+}
 
 ?>
